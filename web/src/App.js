919f4717be76e08496ac95d2e7280951a1fc8925
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2020-03-25 02:58:14 -0300

    server websocket

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer } from "react";
+import React, { useEffect, useState } from "react";
 
 import DevItem from './components/DevItem';
 import DevForm from './components/DevForm';
